527c28b5bb16164bf938e182e8ac0b1a5a19183b
Vivek Machade <vivekmachade323@gmail.com>
2025-01-07 14:50:04 +0530

    multipy function

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,14 @@
 class Calculator{
 public static void main(String[] args){
-System.out.println("Welcome to Calculator"):
+System.out.println("Welcome to Calculator");
 }
 public static int add (int a, int b ){
 return a+b; 
 }
 public static int substract(int a, int b ){
 return a-b; 
+}
+public static int multiply(int a, int b){
+return a*b;
 }
 }
